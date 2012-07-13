5d5eb4ecb63e6720d53f576876d43555df823fc6
Mike Ball <mikedball@gmail.com>
2012-07-12 21:24:50 -0400

    Checking in some commented but non-functional test code.

--- tests/auther.js
+++ tests/auther.js
@@ -13,4 +13,14 @@ describe('auther', function () {
       assert.notEqual(auther, undefined);
     });
   });
+
+
+  //when we send a put request to localhost/some_username with a request body contaning a password and an api key, author will return 200 ok
+  it('returns 200 when we send a PUT request to localhost/someUser', function () {
+    // start up auther server
+    // form the request
+    // send the request
+    // get the response
+    // response body should be 200
+  });
 });
