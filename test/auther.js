4d07bbb74408b44713015c134b13e942de680081
Mike Ball <mikedball@gmail.com>
2012-07-26 20:00:51 -0400

    Created rudimentary test to confirm that auther is running a server.

--- test/auther.js
+++ test/auther.js
@@ -1,4 +1,6 @@
 var assert = require("assert");
+var request = require("request");
+var cp = require("child_process");
 
 describe('auther', function () {
   var auther = require("../auther");
@@ -14,7 +16,15 @@ describe('auther', function () {
     });
   });
 
-
+  it('starts a server that listens on port 4824 by default', function (done) {
+    cp.exec('node ../auther.js', function (err, stdout, stderr) {
+      request('http://localhost:4824', function(err, resp, body) {
+        assert(resp.statusCode === 200);
+        done();
+      });
+    });
+  });
+  
   //when we send a put request to localhost/some_username with a request body contaning a password and an api key, author will return 200 ok
   it('returns 200 when we send a PUT request to localhost/someUser', function () {
     // start up auther server
