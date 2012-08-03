b5f5215685379381ad24d9833d31610695cdcb27
Gabriel Farrell <g@grrawr.com>
2012-08-03 08:58:36 -0400

    Added 401 test and cleaned up

--- test/auther.js
+++ test/auther.js
@@ -2,20 +2,21 @@ var assert = require("assert");
 var request = require("request");
 var fork = require("child_process").fork;
 
-describe('auther', function () {
+describe('auther', function() {
   var child,
-      port = 6666;
+      port = 6666,
+      userUrl = 'http://127.0.0.1:' + port + '/someUser';
 
-  before( function (done) {
-    child = fork('server.js', null, {env: {PORT: port}});
-    child.on('message', function (msg) {
-      if (msg === 'listening') {
+  before(function(done) {
+    child = fork('server.js', [], {env: {PORT: port}});
+    child.on('message', function(msg) {
+      if (msg == 'listening') {
         done();
       }
     });
   });
 
-  after( function () {
+  after(function() {
     child.kill();
   });
 
@@ -27,20 +28,33 @@ describe('auther', function () {
     done();
   });
 
-
-  it('listens on the specified port', function (done) {
-    request('http://127.0.0.1:' +port, function(err, resp, body) {
-      assert(resp.statusCode === 200);
+  it('returns 401 when we send a PUT request with the wrong API key', function(done) {
+    request.put({
+      url: userUrl, 
+      body: {
+        password: 'somePassword',
+        key: 'NOT_SEKRIT'
+      },
+      json: true
+    }, function(err, resp, body) {
+      if (err) throw err;
+      assert(resp.statusCode === 401);
       done();
     });
   });
 
-  //when we send a put request to localhost/some_username with a request body contaning a password and an api key, author will return 200 ok
-  it('returns 200 when we send a PUT request to localhost/someUser', function () {
-    // start up auther server
-    // form the request
-    // send the request
-    // get the response
-    // response body should be 200
+  it('returns 200 when we send a PUT request to localhost/someUser', function(done) {
+    request.put({
+      url: userUrl, 
+      body: {
+        password: 'somePassword',
+        key: 'SEKRIT'
+      },
+      json: true
+    }, function(err, resp, body) {
+      if (err) throw err;
+      assert(resp.statusCode === 200);
+      done();
+    });
   });
 });
